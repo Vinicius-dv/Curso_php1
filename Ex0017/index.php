ccdd1e7c83128cc9c380f8ab2bf0f5c89e09ad49
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-02-01 16:29:34 -0300

    3 desafio completo

---
+++ Ex0017/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Digite um numero</h1>
+    <form action="index.php" method="get">
+        <input type="number" name="raiz" id="">
+        <button>analizar</button>
+    </form>
+
+    <?php 
+    $input_value = $_GET['raiz'] ?? 1;
+    $raiz = number_format(sqrt($input_value),2);
+    $raiz_cubica = number_format($input_value **(1/3),2);
+    ?>
+    <p>Analizando o numero <?php echo$input_value ?>,temos: </p>
+    <p>A sua raiz quadrada é <?php echo $raiz?> </p>
+    <p>A sua raiz cubica é <?php echo $raiz_cubica?> </p>
+</body>
+</html>
